f677f03d442a37735b3f70a6b02f951a7bc0f6a0
Gian Maurício Fritsche <gianmauriciofritsche@MacBook-Air-de-Gian.local>
2022-08-21 15:48:50 -0300

    install and configure bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <div className="App text-center">
+      <h1 className="display-1 text-primary">
+        Learn React
+      </h1>
+    </div>
+  );
 }
 
 export default App;
